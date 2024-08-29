c9011e0b75a52bbe29b1eaa7a2f90ae3e14ef8d2
Daniel Ribeiro <109773063+DevJuvenilson@users.noreply.github.com>
2024-08-29 09:15:39 -0300

    modified js

--- js/secao-compra.js
+++ js/secao-compra.js
@@ -15,45 +15,15 @@ const roupas = [
     { id: 14, imagem: 'assets/images/produtos/vestido-estilo-lengerie.png', name: "VESTIDO", description: 'Vestido Estilo Lengerie', price: 59.90, gender: 'feminino', category: 'vestido' },
     { id: 15, imagem: 'assets/images/produtos/jeans-zw-the-marine.png', name: "JEANS", description: 'Jeans ZW the Marine', price: 69.90, gender: 'masculino', category: 'calça' },
     { id: 16, imagem: 'assets/images/produtos/jaqueta-biker-cropped.png', name: "JAQUETA", description: 'Jaqueta Biker Cropped', price: 79.90, gender: 'masculino', category: 'jaqueta' },
-    { id: 17, imagem: `assets/images/roupas/vestido.jpg`, name: "VESTIDO", description: "Vestido curto de decote redondo com decote em V e manga por debaixo do cotovelo com elástico. Detalhe de bordados perfurados com tecido efeito de caucho. Fecho frontal com botões e cinto no tom.", price: 289, gender: 'feminino', category: 'vestido' },
-    { id: 18, imagem: 'assets/images/roupas/bolsa.jpg"', name: "BOLSA TOTE BAG", description: "Bolsa formato tote bag. Alça de ombro. Detalhe de pendente. Bolso interior maxi. Fecho de ímã. Altura x Largura x Profundidade: 29,5 x 36,5 x 12 cm.", price: 279, gender: 'feminino', category: 'bolsa' },
-    { id: 19, imagem: 'assets/images/roupas/camisa.jpg', name: "CAMISA DE MANGA CURTA", description: "Camisa confeccionada com linho. Gola com lapela, decote em V e manga curta com acabamento em dobra com presilha e botão. Bolsos de patch com aba na frente. Fecho na frente com botões.", price: 199, gender: 'feminino', category: 'camiseta' },
+    { id: 17, imagem: `assets/images/roupas/vestido.jpg`, name: "VESTIDO", description: "Vestido curto de decote redondo com decote em V.", price: 289, gender: 'feminino', category: 'vestido' },
+    { id: 18, imagem: 'assets/images/roupas/bolsa.jpg"', name: "BOLSA TOTE BAG", description: "Bolsa formato tote bag. Alça de ombro.", price: 279, gender: 'feminino', category: 'bolsa' },
+    { id: 19, imagem: 'assets/images/roupas/camisa.jpg', name: "CAMISA DE MANGA CURTA", description: "Camisa confeccionada com linho.", price: 199, gender: 'feminino', category: 'camiseta' },
     { id: 20, imagem: 'produto8.jpg', name: "CALÇA CARGO", description: 'Descrição do produto 20', price:  119.90, gender: 'masculino', category: 'jaqueta' },
     { id: 21, imagem: 'produto9.jpg', name: "CALÇA CARGO", description: 'Descrição do produto 21', price:  129.90, gender: 'feminino', category: 'calça' },
     { id: 22, imagem: 'produto10.jpg', name: "CALÇA CARGO", description: 'Descrição do produto 22', price:  139.90, gender: 'masculino', category: 'camiseta' },
     { id: 23, imagem: 'produto11.jpg', name: "CALÇA CARGO", description: 'Descrição do produto 23', price:  149.90, gender: 'feminino', category: 'jaqueta' },
     { id: 24, imagem: 'produto12.jpg', name: "CALÇA CARGO", description: 'Descrição do produto 24', price:  159.90, gender: 'masculino', category: 'camiseta' },
 ];
-
-// Função para obter o ID do produto a partir da URL
-async function getProductIdFromUrl() {
-    const params = new URLSearchParams(window.location.search);
-    return parseInt(params.get("id"));
-}
-
-// Função para carregar as informações do produto
-async function loadProduct() {
-    const productId = await getProductIdFromUrl();
-    const product = roupas.find(p => p.id === productId);
-    const divDetalhes = document.getElementById("div-conteudo");
-
-    if (product) {
-        document.getElementById("div-imagem").innerHTML = `<img src="${product.imagem}" class="card-img-top" alt="${product.description}">`;
-        divDetalhes.setAttribute('data-price', product.price.toFixed(2))
-        document.getElementById("product-name").textContent = product.name;
-        document.getElementById("product-description").textContent = product.description;
-        document.getElementById("product-price").textContent = product.price.toFixed(2).replace(".", ",");
-        document.getElementById('title').textContent = product.name;
-        document.getElementById('botao-compra').innerHTML = `<a id="link-compra" href="secao-compra.html?id=${product.id}"><button type="button" class="botao-compra open-modal-btn" onclick="openModal()">COMPRE AGORA</button></a>`
-    } else {
-        document.getElementById('title').textContent = "Product not found";
-        document.getElementById("div-imagem").innerHTML = `<img src="assets/images/tratar-erros/error.png" alt="Imagem do Produto">`;
-        divDetalhes.setAttribute('data-price', 0.00);
-        document.getElementById("product-name").textContent = "Product not found";
-        document.getElementById("product-description").textContent = "Sinto muito, não encontramos o produto que você procurava :(";
-        document.getElementById("product-price").textContent = "0,00";
-    }
-}
 
 // Funções para abrir e fechar o modal
 function openModal() {
@@ -125,20 +95,57 @@ function buscarEndereco() {
 function calcCupom() {
     const cupomInput = document.getElementById('cupomInput').value.trim().toUpperCase();
     const totalproduto = document.getElementById('total')
-
-    let desconto = 0
-    if (cupom === 'CUPOM10') {
+    let desconto = 0;
+    
+    if (cupomInput === 'CUPOM10') {
         desconto = 10; 
-    } else if (cupom === 'CUPOM20') {
+    } else if (cupomInput === 'CUPOM20') {
         desconto = 20; 
     } else {
-        alert('Cupom inválido!');
+        desconto = 0;
         return;
     }
 
-    const total = subtotal - desconto;
+    return desconto
+}
 
-    totalproduto.textContent = `R$ ${total.toFixed(2).replace('.', ',')}`;
+// Função para obter o ID do produto a partir da URL
+async function getProductIdFromUrl() {
+    const params = new URLSearchParams(window.location.search);
+    return parseInt(params.get("id"));
+}
+
+// Função para carregar as informações do produto
+async function loadProduct() {
+    const productId = await getProductIdFromUrl();
+    const product = roupas.find(p => p.id === productId);
+    const divDetalhes = document.getElementById("div-conteudo");
+    let subtotal = product.price;  
+    let entrega = product.price * 0.05;
+
+    if (subtotal > 250.89) {
+        entrega = 0;
+    }
+
+    let total = subtotal + entrega;
+
+    if (product) {
+        document.getElementById("div-imagem").innerHTML = `<img src="${product.imagem}" class="card-img-top img-produto" alt="${product.description}">`;
+        divDetalhes.setAttribute('data-price', product.price.toFixed(2));
+        document.getElementById("product-name").textContent = product.name;
+        document.getElementById("product-description").textContent = product.description;
+        document.getElementById("product-price").textContent = `R$${product.price.toFixed(2).replace(".", ",")}`;
+        document.getElementById("subtotal").textContent = `Subtotal: R$${subtotal.toFixed(2).replace(".", ",")}`;
+        document.getElementById("entrega").textContent = `Entrega: R$${entrega.toFixed(2).replace(".", ",")}`;
+        document.getElementById("total").textContent = `Total: R$${total.toFixed(2).replace(".", ",")}`;
+    } else {
+        document.getElementById('title').textContent = "Product not found";
+        document.getElementById("div-imagem").innerHTML = `<img src="assets/images/tratar-erros/error.png" alt="Imagem do Produto">`;
+        divDetalhes.setAttribute('data-price', 0.00);
+        document.getElementById("product-name").textContent = "Product not found";
+        document.getElementById("product-description").textContent = "Sinto muito, não encontramos o produto que você procurava :(";
+        document.getElementById("product-price").textContent = "0,00";
+    }
 }
 
 // Executa a função quando a página é carregada
